3e602aa6f587fa8e02328d127cdaea5fef5e0fc9
SunRoute <sunroute1974@gmail.com>
2022-10-21 13:31:09 +0200

    Otros incompleto

--- scriptsPrueba/Instalar_FTP.sh
+++ scriptsPrueba/Instalar_FTP.sh
@@ -6,10 +6,10 @@ sed -i 's/write_enable=.*/write_enable=YES/' /etc/vsftpd.conf
 sed -i 's/local_umask=.*/local_umask=022/' /etc/vsftpd.conf
 sed -i 's/chroot_local_user=.*/chroot_local_user=YES/' /etc/vsftpd.conf
 sed -i 's/chroot_list_enable=.*/chroot_list_enable=YES/' /etc/vsftpd.conf
-sed -i 's/chroot_list_file=.*/chroot_list_file=/etc/vsftpd.chroot_list/' /etc/vsftpd.conf
+sed -i 's/chroot_list_file=.*/chroot_list_file=\/etc\/vsftpd.chroot_list/' /etc/vsftpd.conf
 
-# read -p  "Ingresa un nombre de usuario para la conexión (minusculas y sin espacios):"  USER
+read -p  "Ingresa un nombre de usuario para la conexión (minúsculas y sin espacios):"  USER
 
-sed -i "s/$USER/" /etc/vsftpd.chroot_list
+echo "$USER" | tee -a /etc/vsftpd.chroot_list
 
 systemctl restart vsftpd
