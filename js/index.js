c1af0066e89f1f92c1e6d3198ddb850bd7495097
SunRoute <sunroute1974@gmail.com>
2022-11-29 14:19:02 +0100

    notificacion incompleto

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import {renderBotonMasMenos} from './boton-mas-menos.js';
 import {renderEtiquetas} from './etiquetas.js';
-import {renderContadorTexto } from './input.js'
+import {renderContadorTexto } from './input.js';
+import { renderNotificacion } from "./notificacion.js";
 
 renderBotonMasMenos();
 renderEtiquetas();
 renderContadorTexto();
+renderNotificacion();
